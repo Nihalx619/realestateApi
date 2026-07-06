adb7ab15f489bfebefbe37e6e715f0f4815a4b85
Nihal <nihalguptax@gmail.com>
2026-07-06 07:43:59 +0530

    Fix enquiry hang - async email with SMTP timeouts

--- src/main/java/com/realestate/realestateapi/service/EmailService.java
+++ src/main/java/com/realestate/realestateapi/service/EmailService.java
@@ -3,6 +3,7 @@ package com.realestate.realestateapi.service;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import com.realestate.realestateapi.entity.Enquiry;
@@ -21,7 +22,8 @@ public class EmailService {
     public EmailService(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
-
+    
+    @Async
     public void sendEnquiryNotification(Enquiry enquiry) {
         try {
             SimpleMailMessage message = new SimpleMailMessage();
